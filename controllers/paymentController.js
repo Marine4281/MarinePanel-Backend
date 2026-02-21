37a15a823a034359e9917a17a959ca54d8cbe20b
Marine4281 <marinepro360@gmail.com>
2026-02-22 00:11:51 +0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -23,6 +23,7 @@ export const initializePaystack = async (req, res) => {
       amount,
       status: "Pending",
       type: "Deposit",
+      method, 
     });
 
     const response = await axios.post(

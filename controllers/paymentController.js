4c0f7588840293624b66cc7945b70e7c97248a6f
Marine4281 <marinepro360@gmail.com>
2026-02-22 00:09:05 +0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -9,7 +9,7 @@ import Wallet from "../models/Wallet.js";
 export const initializePaystack = async (req, res) => {
   try {
     const { amount, method } = req.body;
-    if (!amount || amount <= 0) {
+    if (!amount || amount <= 0 || method) {
       return res.status(400).json({ message: "Invalid amount or method" });
     }
 

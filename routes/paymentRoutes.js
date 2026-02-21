64b943bf30c9efc35d5bd4bca354788bdcd1bcc4
Marine4281 <marinepro360@gmail.com>
2026-02-21 12:50:38 +0300

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { handlePaystackWebhook } from "../controllers/WebHookController.js";
+import { handlePaystackWebhook } from "../controllers/WebhookController.js";
 import { addFunds, getWallet } from "../controllers/WalletController.js";
 import { getUserPaymentMethods } from "../controllers/PaymentMethodsController.js";
 import { protect } from "../middlewares/authMiddleware.js"; // If you have auth

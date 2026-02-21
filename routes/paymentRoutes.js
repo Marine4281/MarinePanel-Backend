1e49e389126477b254df3fea51711a02329bb63f
Marine4281 <marinepro360@gmail.com>
2026-02-21 15:26:21 +0300

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,6 +2,14 @@ import express from "express";
 import { handlePaystackWebhook } from "../controllers/WebhookController.js";
 import { addFunds, getWallet } from "../controllers/walletController.js";
 import { getUserPaymentMethods } from "../controllers/paymentMethodController.js";
-import { protect } from "../middlewares/authMiddleware.js"; // If you have auth
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+
+// your routes here
+// router.post("/webhook/paystack", handlePaystackWebhook);
+// router.post("/addfunds", protect, addFunds);
+// router.get("/wallet", protect, getWallet);
+// etc...
+
+export default router;

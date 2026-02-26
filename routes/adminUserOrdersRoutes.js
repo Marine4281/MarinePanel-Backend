d5085359cbd26732ea73ab1c5990028188137bde
Marine4281 <marinepro360@gmail.com>
2026-02-26 21:42:11 +0300

    Update adminUserOrdersRoutes.js

--- routes/adminUserOrdersRoutes.js
+++ routes/adminUserOrdersRoutes.js
@@ -2,13 +2,19 @@ import express from "express";
 import {
   getUserOrders,
   updateOrderStatus,
+  updateOrderProgress,   // 👈 ADD THIS
   refundOrder,
 } from "../controllers/AdminUserOrdersController.js";
 
 const router = express.Router();
 
 router.get("/", getUserOrders);
+
 router.post("/:id/status", updateOrderStatus);
+
+// 👇 ADD THIS ROUTE
+router.patch("/:id/progress", updateOrderProgress);
+
 router.post("/:id/refund", refundOrder);
 
 export default router;

10ac886048e16c955a0823f0df72a8584f0f45a7
Marine4281 <marinepro360@gmail.com>
2026-03-12 22:16:26 +0300

    Update adminSettingsRoutes.js

--- routes/adminSettingsRoutes.js
+++ routes/adminSettingsRoutes.js
@@ -3,14 +3,37 @@ import {
   getCommission,
   updateCommission,
   resetRevenue,
+  getResellerSettings,
+  updateResellerSettings,
 } from "../controllers/AdminSettingsController.js";
+
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(protect); // only authenticated admins can access
+
+/*
+--------------------------------
+Commission Settings
+--------------------------------
+*/
 router.get("/commission", getCommission);
 router.put("/commission", updateCommission);
+
+/*
+--------------------------------
+Reseller Platform Settings
+--------------------------------
+*/
+router.get("/reseller", getResellerSettings);
+router.put("/reseller", updateResellerSettings);
+
+/*
+--------------------------------
+Reset Revenue
+--------------------------------
+*/
 router.post("/reset-revenue", resetRevenue);
 
 export default router;

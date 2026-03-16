283077b85820a10b230a2fbf0111a5a04a84baa9
Marine4281 <marinepro360@gmail.com>
2026-03-16 17:41:20 +0300

    Update brandingRoutes.js

--- routes/brandingRoutes.js
+++ routes/brandingRoutes.js
@@ -1,14 +1,26 @@
+// routes/brandingRoutes.js
+
 import express from "express";
 import { getBranding } from "../controllers/brandingController.js";
-import { updateBranding } from "../controllers/resellerController.js"; // make sure this import is correct
-import { protect } from "../middlewares/authMiddleware.js"; // optional auth
+import { updateBranding } from "../controllers/resellerController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// GET branding
+/*
+--------------------------------
+Get Branding (Public)
+Used by frontend to detect
+brand name, logo, theme color
+--------------------------------
+*/
 router.get("/", getBranding);
 
-// POST branding update
-router.post("/", protect, updateBranding); // <-- this allows POST /api/reseller/branding
+/*
+--------------------------------
+Update Branding (Reseller Only)
+--------------------------------
+*/
+router.patch("/", protect, updateBranding);
 
 export default router;

5933ae5543d03b08994a607e33bbe043c4d157c3
Marine4281 <marinepro360@gmail.com>
2026-04-02 22:45:59 +0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,34 +1,16 @@
 import express from "express";
-import {
-  getUsers,
-  getUserById,
-  updateUserBalance,
-  promoteToAdmin,
-  demoteFromAdmin,
-  getUserTransactions
-} from "../controllers/userController.js";
+import { getUsers, getOrders, getStats } from "../controllers/adminController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { adminOnly } from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
-// Protect all routes to admin
+// Only admin users can access these routes
 router.use(protect, adminOnly);
 
-// Get all users
-router.get("/users", getUsers);
-
-// Get single user (with transactions)
-router.get("/users/:id", getUserById);
-
-// Update user balance
-router.put("/users/:id/balance", updateUserBalance);
-
-// Promote / demote user
-router.patch("/users/:id/promote", promoteToAdmin);
-router.patch("/users/:id/demote", demoteFromAdmin);
-
-// Get user transactions
-router.get("/users/:id/transactions", getUserTransactions);
+// Admin routes
+router.get("/users", getUsers);            // Get all users
+router.get("/orders", getOrders);          // Get orders separately if needed
+router.get("/stats", getStats);            // Get stats + revenue + recent orders
 
 export default router;

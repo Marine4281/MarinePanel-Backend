6a6bd43fde11c9318719b925480d38ef2ac0b288
Marine4281 <marinepro360@gmail.com>
2026-04-02 20:58:05 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProfile, updateProfile, promoteToAdmin } from "../controllers/userController.js";
+import { getProfile, updateProfile, promoteToAdmin, demoteFromAdmin } from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { adminOnly } from "../middlewares/adminMiddleware.js";
 
@@ -9,9 +9,8 @@ const router = express.Router();
 router.get("/profile", protect, getProfile);
 router.put("/profile", protect, updateProfile);
 
-// ✅ Admin-only route to promote a user
+// ✅ Admin-only routes
 router.patch("/:id/promote", protect, adminOnly, promoteToAdmin);
-// Demote admin to user
 router.patch("/:id/demote", protect, adminOnly, demoteFromAdmin);
 
 export default router;

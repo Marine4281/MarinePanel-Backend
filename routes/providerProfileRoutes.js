e582d9fbfca8e94499ad2ea93b4cb4c76375d8f7
Marine4281 <marinepro360@gmail.com>
2026-04-07 00:00:51 +0300

    Update providerProfileRoutes.js

--- routes/providerProfileRoutes.js
+++ routes/providerProfileRoutes.js
@@ -3,6 +3,8 @@ import express from "express";
 import {
   createProviderProfile,
   getProviderProfiles,
+  getProviderProfileById,
+  updateProviderProfile,
   deleteProviderProfile,
 } from "../controllers/ProviderProfileController.js";
 
@@ -13,6 +15,12 @@ router.post("/profiles", createProviderProfile);
 
 // GET all providers
 router.get("/profiles", getProviderProfiles);
+
+// GET single provider by ID
+router.get("/profiles/:id", getProviderProfileById);
+
+// UPDATE provider
+router.put("/profiles/:id", updateProviderProfile);
 
 // DELETE provider
 router.delete("/profiles/:id", deleteProviderProfile);

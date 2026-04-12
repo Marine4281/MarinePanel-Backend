ea9af8daab287002c2cd546a429dae9384f1381c
Marine4281 <marinepro360@gmail.com>
2026-04-12 10:18:51 +0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createOrder, getMyOrders, previewOrder } from "../controllers/orderController.js";
+import { createOrder, getMyOrders, getMyOrdersStats, previewOrder } from "../controllers/orderController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createOrder);
 router.get("/my-orders", protect, getMyOrders);
+router.get("/orders/my-orders/stats", protect, getMyOrdersStats);
 router.post("/preview", protect, previewOrder); // <-- new endpoint
 
 export default router;

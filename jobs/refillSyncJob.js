27b023ff6b29be988986e4e757eb064c94681039
Marine4281 <marinepro360@gmail.com>
2026-04-23 13:23:26 +0300

    Update refillSyncJob.js

--- jobs/refillSyncJob.js
+++ jobs/refillSyncJob.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { syncProviderRefills } from "../services/providerRefillSync.js";
+import { syncProviderRefills } from "../services /providerRefillSync.js";
 
 export const startRefillSyncJob = () => {
   console.log("⏱️ Refill sync cron started");

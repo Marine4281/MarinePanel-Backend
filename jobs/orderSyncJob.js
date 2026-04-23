27c0886ae696fb59f3ed0cad8f50ca5be69712c2
Marine4281 <marinepro360@gmail.com>
2026-04-23 13:20:41 +0300

    Update orderSyncJob.js

--- jobs/orderSyncJob.js
+++ jobs/orderSyncJob.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { syncProviderOrders } from "../services/providerStatusSync.js";
+import { syncProviderOrders } from "../services /providerStatusSync.js";
 
 export const startOrderSyncJob = (io) => {
   console.log("⏱️ Order sync cron started");

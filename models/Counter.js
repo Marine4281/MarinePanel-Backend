8425e3d51a79dcb637f29a619465544f1a2048b0
Marine4281 <marinepro360@gmail.com>
2026-04-23 15:06:23 +0300

    Update Counter.js

--- models/Counter.js
+++ models/Counter.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 const counterSchema = new mongoose.Schema({
-  id: {
+  _id: {
     type: String,
     required: true,
-    unique: true,
   },
   seq: {
     type: Number,
-    default: 1000, // services will start from 1001
+    default: 1000,
   },
 });
 

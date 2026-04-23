4ebc6aea58b19d0d31039029ef58ae1f592feb9a
Marine4281 <marinepro360@gmail.com>
2026-04-23 15:38:11 +0300

    Update orderId.js

--- utils/orderId.js
+++ utils/orderId.js
@@ -1,10 +1,6 @@
 // utils/orderId.js
-
 import Counter from "../models/Counter.js";
 
-/**
- * Generate next Order ID (1001, 1002, 1003...)
- */
 export const getNextOrderId = async () => {
   try {
     const counter = await Counter.findOneAndUpdate(
@@ -13,9 +9,7 @@ export const getNextOrderId = async () => {
       { new: true, upsert: true }
     );
 
-    // 🔥 FIX: start from 1001
-    return 1206 + counter.seq;
-
+    return counter.seq; // ✅ clean sequential ID
   } catch (error) {
     console.error("Error generating Order ID:", error);
     throw new Error("Failed to generate Order ID");

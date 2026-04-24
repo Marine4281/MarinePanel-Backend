5657d9a6f4f777aacf75725717da96594936550c
Marine4281 <marinepro360@gmail.com>
2026-04-25 00:01:43 +0300

    Update ServiceSettings.js

--- models/ServiceSettings.js
+++ models/ServiceSettings.js
@@ -1,17 +1,8 @@
 import mongoose from "mongoose";
 
-const serviceSettingsSchema = new mongoose.Schema(
-  {
-    globalRefillEnabled: {
-      type: Boolean,
-      default: true,
-    },
-    globalCancelEnabled: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { timestamps: true }
-);
+const serviceSettingsSchema = new mongoose.Schema({
+  globalRefillEnabled: { type: Boolean, default: true },
+  globalCancelEnabled: { type: Boolean, default: true },
+});
 
 export default mongoose.model("ServiceSettings", serviceSettingsSchema);

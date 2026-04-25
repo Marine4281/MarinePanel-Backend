59365b1222fa57b58f885a46303f6f4ba3695ef8
Marine4281 <marinepro360@gmail.com>
2026-04-25 11:51:40 +0300

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,9 +1,12 @@
+//routes/serviceRoutes.js
 import express from "express";
 import { getServicesPublic } from "../controllers/serviceController.js";
+import { getServiceSettings } from "../controllers/serviceTogglesController.js";
 
 const router = express.Router();
 
 // Public route
 router.get("/", getServicesPublic);
+router.get("/service-settings", getServiceSettings);
 
 export default router;

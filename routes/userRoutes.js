39922e65f55e5f00a0580cba99e2fcd2668aa775
Marine4281 <marinepro360@gmail.com>
2026-04-29 17:55:28 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 //routes/userRoutes.js
 import express from "express";
-import { getProfile, updateProfile } from "../controllers/userController.js";
+import { getProfile, updateProfile ,generateApiKey, revokeApiKey } from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js"; // JWT auth middleware
 
 const router = express.Router();
@@ -8,5 +8,8 @@ const router = express.Router();
 // ✅ Use JWT to get user, no :id needed
 router.get("/profile", protect, getProfile);
 router.put("/profile", protect, updateProfile);
+
+router.post("/generate-api-key", protect, generateApiKey);
+router.post("/revoke-api-key", protect, revokeApiKey);
 
 export default router;

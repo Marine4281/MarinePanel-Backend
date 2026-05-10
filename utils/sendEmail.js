f16a2ba6a473e779621a1a96e8b6c9aedd2f4463
Marine4281 <marinepro360@gmail.com>
2026-05-10 19:57:13 +0300

    Refactor sendEmail to use environment variables

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,30 @@
+// utils/sendEmail.js
+
+import nodemailer from "nodemailer";
+
 // sendEmail({ to, subject, text })
 const sendEmail = async ({ to, subject, text }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",       // Gmail SMTP
+      host: "smtp.gmail.com",
       port: 587,
-      secure: false,                // false for 587
+      secure: false, // false for port 587
       auth: {
-        user: "marinepanel6@gmail.com",  // EMAIL_USER
-        pass: "pzae gqke djjz vxeo",        // EMAIL_PASS (App password)
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const info = await transporter.sendMail({
-      from: `"Marine Panel" <marinepanel6@gmail.com>`, // EMAIL_FROM
+      from: `"Marine Panel" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
     });
 
-    console.log("Email sent: %s", info.messageId);
+    console.log("Email sent:", info.messageId);
+
+    return info;
   } catch (error) {
     console.error("Email error:", error);
     throw new Error("Email could not be sent");

71c3870ef8b8bfdc1b0342674b188550f9011dfb
Marine4281 <marinepro360@gmail.com>
2026-05-17 16:06:20 +0300

    Update cpOwnerFinancialRoutes.js

--- routes/cpOwnerFinancialRoutes.js
+++ routes/cpOwnerFinancialRoutes.js
@@ -1,4 +1,3 @@
-// routes/cpOwnerFinancialRoutes.js
 import express from "express";
 import {
   getCPFinancialSummary,
@@ -6,15 +5,22 @@ import {
   getCPFinancialUsers,
   getCPWithdrawals,
   getCPResellerEarnings,
+  cpApproveWithdrawal,
+  cpRejectWithdrawal,
+  cpSetWithdrawalStatus,
 } from "../controllers/cpOwnerFinancialController.js";
 
 const router = express.Router();
-// Auth + cpOwnerOnly applied in app.js
 
 router.get("/summary",           getCPFinancialSummary);
 router.get("/profit",            getCPProfit);
 router.get("/users",             getCPFinancialUsers);
 router.get("/withdrawals",       getCPWithdrawals);
 router.get("/reseller-earnings", getCPResellerEarnings);
+
+// Reseller withdrawal actions
+router.post("/withdrawals/:userId/:txId/approve", cpApproveWithdrawal);
+router.post("/withdrawals/:userId/:txId/reject",  cpRejectWithdrawal);
+router.patch("/withdrawals/:userId/:txId/status", cpSetWithdrawalStatus);
 
 export default router;

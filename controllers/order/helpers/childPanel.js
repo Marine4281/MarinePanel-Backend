ac45a07896e0bd235991dda716cde284803cbe80
Marine4281 <marinepro360@gmail.com>
2026-05-22 18:30:42 +0300

    Update childPanel.js

--- controllers/order/helpers/childPanel.js
+++ controllers/order/helpers/childPanel.js
@@ -1,8 +1,8 @@
+// controllers/order/helpers/childPanel.js
 import User from "../../../models/User.js";
 
 export const resolveChildPanelData = async (user) => {
   let childPanelOwnerId = null;
-  let childPanelCommission = 0;
   let childPanelPerOrderFee = 0;
 
   if (user.childPanelOwner) {
@@ -10,20 +10,9 @@ export const resolveChildPanelData = async (user) => {
 
     if (cpOwner && cpOwner.isChildPanel && cpOwner.childPanelIsActive) {
       childPanelOwnerId = cpOwner._id;
-
-      childPanelPerOrderFee = Number(
-        cpOwner.childPanelPerOrderFee || 0
-      );
-
-      childPanelCommission = Number(
-        cpOwner.childPanelCommissionRate || 0
-      );
+      childPanelPerOrderFee = Number(cpOwner.childPanelPerOrderFee || 0);
     }
   }
 
-  return {
-    childPanelOwnerId,
-    childPanelCommission,
-    childPanelPerOrderFee,
-  };
+  return { childPanelOwnerId, childPanelPerOrderFee };
 };

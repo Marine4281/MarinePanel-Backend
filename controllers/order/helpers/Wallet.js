3e956ed9f48be5a95eb533c66f14043e00ef1618
Marine4281 <marinepro360@gmail.com>
2026-05-22 13:07:56 +0300

    Create Wallet.js

---
+++ controllers/order/helpers/Wallet.js
@@ -0,0 +1,25 @@
+import Wallet from "../../models/Wallet.js";
+import User from "../../models/User.js";
+
+export const calculateBalance = (transactions = []) =>
+  transactions.reduce((acc, t) => acc + (t.amount || 0), 0);
+
+export const ensureWallet = async (userId) => {
+  let wallet = await Wallet.findOne({ user: userId });
+
+  if (!wallet) {
+    wallet = await Wallet.create({
+      user: userId,
+      balance: 0,
+      transactions: [],
+    });
+  }
+
+  return wallet;
+};
+
+export const updateUserBalance = async (userId, wallet) => {
+  await User.findByIdAndUpdate(userId, {
+    balance: wallet.balance,
+  });
+};

b47f93db8f6c335c74d02fdd3dc5d2ff23198627
Marine4281 <marinepro360@gmail.com>
2026-05-25 23:58:46 +0300

    Simplify cpAdminLogRoutes by removing middleware
    Removed authentication middleware from the route.

--- routes/cpAdminLogRoutes.js
+++ routes/cpAdminLogRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { getCpAdminLogs } from "../controllers/cpAdminLogController.js";
-import { protect } from "../middlewares/authMiddleware.js";
-import { childPanelOnly } from "../middlewares/childPanelMiddleware.js"; // your existing CP guard
 
 const router = express.Router();
 
-router.get("/", protect, childPanelOnly, getCpAdminLogs);
+// Auth + cpOwnerOnly applied in app.js
+router.get("/", getCpAdminLogs);
 
 export default router;

875c593d8e67a50266de84f87904f7ff37db3cca
Marine4281 <marinepro360@gmail.com>
2026-05-26 00:12:15 +0300

    Remove unnecessary comments and fix export syntax

--- routes/cpAdminLogRoutes.js
+++ routes/cpAdminLogRoutes.js
@@ -2,8 +2,5 @@ import express from "express";
 import { getCpAdminLogs } from "../controllers/cpAdminLogController.js";
 
 const router = express.Router();
-
-// Auth + cpOwnerOnly applied in app.js
 router.get("/", getCpAdminLogs);
-
 export default router;

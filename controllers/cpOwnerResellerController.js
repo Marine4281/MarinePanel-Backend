eea359b40b39dcfeff7de783ad425280f4da4c81
Marine4281 <marinepro360@gmail.com>
2026-05-26 20:12:27 +0300

    Update cpOwnerResellerController.js

--- controllers/cpOwnerResellerController.js
+++ controllers/cpOwnerResellerController.js
@@ -9,7 +9,7 @@ import User from "../models/User.js";
 import Order from "../models/Order.js";
 import Wallet from "../models/Wallet.js";
 import mongoose from "mongoose";
-import logAdminAction from "../utils/logAdminAction.js";
+import logCpAdminAction from "../utils/logCpAdminAction.js";
 
 // ======================= HELPERS =======================
 
@@ -239,7 +239,7 @@ export const toggleCPResellerStatus = async (req, res) => {
         { $set: { isSuspended: true } }
       );
     }
-    logAdminAction({ adminId: req.user._id, adminEmail: req.user.email, action: "TOGGLE_RESELLER_STATUS", targetType: "Reseller", targetId: reseller._id, description: `Toggled reseller ${reseller._id} status`, ipAddress: req.ip }).catch(() => {});
+    logCpAdminAction({ adminId: req.user._id, adminEmail: req.user.email,childPanelId: req.user._id, action: "TOGGLE_RESELLER_STATUS", targetType: "Reseller", targetId: reseller._id, description: `Toggled reseller ${reseller._id} status`, ipAddress: req.ip }).catch(() => {});
 
     res.json({
       success: true,
@@ -284,7 +284,7 @@ export const updateCPResellerCommission = async (req, res) => {
     reseller.resellerCommissionRate = rate;
     reseller.commissionUpdatedAt = new Date();
     await reseller.save();
-    logAdminAction({ adminId: req.user._id, adminEmail: req.user.email, action: "UPDATE_COMMISSION", targetType: "Reseller", targetId: reseller._id, description: `Updated reseller ${reseller._id} commission`, ipAddress: req.ip }).catch(() => {});
+    logCpAdminAction({ adminId: req.user._id, adminEmail: req.user.email, childPanelId: req.user._id,action: "UPDATE_COMMISSION", targetType: "Reseller", targetId: reseller._id, description: `Updated reseller ${reseller._id} commission`, ipAddress: req.ip }).catch(() => {});
 
     res.json({ success: true, message: "Commission updated" });
   } catch (error) {
@@ -353,7 +353,7 @@ export const updateCPResellerBalance = async (req, res) => {
     await wallet.save();
 
     await User.findByIdAndUpdate(reseller._id, { balance: wallet.balance });
-    logAdminAction({ adminId: req.user._id, adminEmail: req.user.email, action: "UPDATE_BALANCE", targetType: "Reseller", targetId: reseller._id, description: `Updated reseller ${reseller._id} balance`, ipAddress: req.ip }).catch(() => {});
+logCpAdminAction adminId: req.user._id, adminEmail: req.user.email, childPanelId: req.user._id,action: "UPDATE_BALANCE", targetType: "Reseller", targetId: reseller._id, description: `Updated reseller ${reseller._id} balance`, ipAddress: req.ip }).catch(() => {});
 
     res.json({ success: true, message: "Balance updated", balance: wallet.balance });
   } catch (error) {

c87ae31312647e0561034522f78f0b3f7ea600da
Marine4281 <marinepro360@gmail.com>
2026-06-21 19:32:33 +0300

    Fix import statement for getDashboardBranding

--- routes/brandingRoutes.js
+++ routes/brandingRoutes.js
@@ -4,7 +4,7 @@ import express from "express";
 import {
   getPublicBranding,
   updateResellerLandingTemplate,
-  getDashboardBranding
+  getDashboardBranding,
 } from "../controllers/brandingController.js";
 
 import { updateBranding } from "../controllers/brandingController.js";

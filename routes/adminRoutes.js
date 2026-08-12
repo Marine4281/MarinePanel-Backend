73aca254e606b843b0c5897ce91b250ae65d2cbf
Marine4281 <marinepro360@gmail.com>
2026-08-12 22:08:00 +0300

    Add revenue trend and top performers routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getUsers, getStats } from "../controllers/adminController.js";
+import {
+  getUsers,
+  getStats,
+  getRevenueTrend,
+  getTopPerformers,
+} from "../controllers/adminController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { adminOnly } from "../middlewares/adminMiddleware.js";
 
@@ -9,7 +14,9 @@ const router = express.Router();
 router.use(protect, adminOnly);
 
 // Admin routes
-router.get("/users", getUsers);            // Get all users
-router.get("/stats", getStats);            // Get stats + revenue + recent orders
+router.get("/users", getUsers);                    // Get all users
+router.get("/stats", getStats);                     // Get stats + revenue + recent orders
+router.get("/revenue-trend", getRevenueTrend);       // Revenue trend chart data
+router.get("/top-performers", getTopPerformers);     // Top platforms/categories/services
 
 export default router;
